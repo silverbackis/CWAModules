0b1ca9dbcd72a1af34ea1bc7a7fe08831204a18e
Daniel West <daniel@silverback.is>
2018-10-18 19:02:05 +0100

    Login - add mixin for reuse

--- packages/bulma/lib/core/pages/login.vue
+++ packages/bulma/lib/core/pages/login.vue
@@ -46,70 +46,21 @@
 </template>
 
 <script>
-  import { mapGetters, mapMutations, mapState } from 'vuex'
-  import FormTag from '~/.nuxt/bwstarter/components/Form/Form'
-  import FormInput from '~/.nuxt/bwstarter/components/Form/FormInput'
-  import FormMixin from '~/.nuxt/bwstarter/components/Form/_Mixin'
-  import { Utilities } from '~/.nuxt/bwstarter/core/server'
+  import LoginMixin from '~/.nuxt/bwstarter/components/Form/_LoginMixin'
 
   export default {
-    mixins: [ FormMixin ],
-    components: {
-      FormTag,
-      FormInput
-    },
+    mixins: [ LoginMixin ],
     head: {
       title: 'Admin Login',
       meta: [
         { hid: 'description', name: 'description', content: '' }
       ]
     },
-    computed: {
-      ...mapGetters({
-        getApiUrl: 'bwstarter/getApiUrl'
-      }),
-      ...mapState({ token: state => state.bwstarter.token }),
-      formErrors () {
-        return this.storeForm ? this.storeForm.vars.errors : []
-      }
-    },
-    methods: {
-      ...mapMutations({
-        setAuthToken: 'setAuthToken',
-        addNotification: 'notifications/addNotification'
-      }),
-      formSuccess (data) {
-        if (data.token) {
-          this.$bwstarter.$storage.setState('token', data.token)
-          this.$router.replace('/')
-        }
-      }
-    },
     mounted () {
       if (this.token) {
         this.$router.replace('/')
         this.addNotification('You are already logged in')
       }
-    },
-    async asyncData ({ store: { dispatch, getters }, app: { $axios, $bwstarter }, res }) {
-      let response = await $bwstarter.fetchAndStoreLayout(null, true)
-
-      if (process.server) {
-        Utilities.setResponseCookies(res, response)
-      }
-
-      try {
-        let { data: { form } } = await $axios.get('login_form')
-        form.vars.action = '/login'
-        return {
-          form
-        }
-      } catch (err) {
-        console.error('Could not load form', err)
-      }
-    },
-    beforeDestroy () {
-      this.destroyForm(this.formId)
     }
   }
 </script>
---
+++ packages/components/lib/core/Form/_LoginMixin.js
@@ -0,0 +1,54 @@
+import FormMixin from '~/.nuxt/bwstarter/components/Form/_Mixin'
+import { mapGetters, mapState, mapMutations } from 'vuex'
+import { Utilities } from '~/.nuxt/bwstarter/core/server'
+import FormTag from '~/.nuxt/bwstarter/components/Form/Form'
+import FormInput from '~/.nuxt/bwstarter/components/Form/FormInput'
+
+export default {
+  mixins: [ FormMixin ],
+  components: {
+    FormTag,
+    FormInput
+  },
+  computed: {
+    ...mapGetters({
+      getApiUrl: 'bwstarter/getApiUrl'
+    }),
+    ...mapState({ token: state => state.bwstarter.token }),
+    formErrors () {
+      return this.storeForm ? this.storeForm.vars.errors : []
+    }
+  },
+  methods: {
+    ...mapMutations({
+      setAuthToken: 'setAuthToken',
+      addNotification: 'notifications/addNotification'
+    }),
+    formSuccess (data) {
+      if (data.token) {
+        this.$bwstarter.$storage.setState('token', data.token)
+        this.$router.replace('/')
+      }
+    }
+  },
+  async asyncData ({ store: { dispatch, getters }, app: { $axios, $bwstarter }, res }) {
+    let response = await $bwstarter.fetchAndStoreLayout(null, true)
+
+    if (process.server) {
+      Utilities.setResponseCookies(res, response)
+    }
+
+    try {
+      let { data: { form } } = await $axios.get('login_form')
+      form.vars.action = '/login'
+      return {
+        form
+      }
+    } catch (err) {
+      console.error('Could not load form', err)
+    }
+  },
+  beforeDestroy () {
+    this.destroyForm(this.formId)
+  }
+}
