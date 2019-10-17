a136ef958e21a898ccfe725acc042601f8e16a56
Daniel West <daniel@silverback.is>
2019-10-17 11:00:34 +0100

    Split Collection Component - Collection Pagniation and Collection Admin components

--- packages/bulma/lib/core/components/Collection/Collection.vue
+++ packages/bulma/lib/core/components/Collection/Collection.vue
@@ -34,106 +34,19 @@
           </div>
         </div>
       </div>
-      <nav
-        v-if="!disabledPagination && paginationData.last > 1"
-        :class="['pagination is-rounded', { 'is-loading': reloading }]"
-        role="navigation"
-        aria-label="pagination"
-      >
-        <a
-          class="pagination-previous"
-          :disabled="
-            !paginationData.previous || page === paginationData.previous
-          "
-          @click="goToPage(paginationData.previous)"
-          >Previous</a
-        >
-        <a
-          class="pagination-next"
-          :disabled="!paginationData.next || page === paginationData.next"
-          @click="goToPage(paginationData.next)"
-          >Next</a
-        >
-        <ul class="pagination-list">
-          <li v-if="page !== paginationData.first">
-            <a
-              class="pagination-link"
-              :aria-label="goToPageLabel(paginationData.first)"
-              @click="goToPage(paginationData.first)"
-              >{{ paginationData.first }}</a
-            >
-          </li>
-
-          <template
-            v-if="
-              paginationData.previous &&
-                paginationData.previous !== paginationData.first
-            "
-          >
-            <li><span class="pagination-ellipsis">&hellip;</span></li>
-            <li>
-              <a
-                class="pagination-link"
-                :aria-label="goToPageLabel(paginationData.previous)"
-                >{{ paginationData.previous }}</a
-              >
-            </li>
-          </template>
-
-          <li>
-            <a
-              class="pagination-link is-current"
-              :aria-label="goToPageLabel(page)"
-              aria-current="page"
-              >{{ page }}</a
-            >
-          </li>
-
-          <template
-            v-if="
-              paginationData.next && paginationData.next !== paginationData.last
-            "
-          >
-            <li>
-              <a
-                class="pagination-link"
-                :aria-label="goToPageLabel(paginationData.next)"
-                >{{ paginationData.next }}</a
-              >
-            </li>
-            <li><span class="pagination-ellipsis">&hellip;</span></li>
-          </template>
-
-          <li v-if="page !== paginationData.last">
-            <a
-              class="pagination-link"
-              :aria-label="goToPageLabel(paginationData.last)"
-              @click="goToPage(paginationData.last)"
-              >{{ paginationData.last }}</a
-            >
-          </li>
-        </ul>
-      </nav>
-
-      <div v-if="!disabledAdmin && $bwstarter.isAdmin">
-        <button
-          class="button is-primary is-fullwidth"
-          :class="{ 'is-loading': adding }"
-          @click="addCollectionItem"
-        >
-          <span class="icon is-small">
-            <font-awesome-icon icon="plus" />
-          </span>
-          <span>Add New</span>
-        </button>
-        <div class="reload-link-row has-text-centered">
-          <a
-            class="reload-link has-text-grey-light"
-            @click.prevent="reloadCollection"
-            >reload collection</a
-          >
-        </div>
-      </div>
+      <collection-pagination
+        :pagination-data="paginationData"
+        :disabled-pagination="disabledPagination"
+        :page="page"
+        @goToPage="goToPage"
+        @goToPageLabel="goToPageLabel"
+      />
+      <collection-admin
+        :disabled-admin="disabledAdmin"
+        :adding="adding"
+        @addCollectionItem="addCollectionItem"
+        @reloadCollection="reloadCollection"
+      />
     </div>
   </component-wrapper>
 </template>
@@ -142,8 +55,14 @@
 import { mapGetters } from 'vuex'
 import { name as entitiesModuleName } from '~/.nuxt/bwstarter/core/storage/entities'
 import ComponentMixin from '~/.nuxt/bwstarter/bulma/components/componentMixin'
+import CollectionPagination from './CollectionPagination'
+import CollectionAdmin from './CollectionAdmin'
 
 export default {
+  components: {
+    CollectionPagination,
+    CollectionAdmin
+  },
   mixins: [ComponentMixin],
   props: {
     isCentered: {
