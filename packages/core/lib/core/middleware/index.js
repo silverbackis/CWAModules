0f6b4a6bb3548d9fac1489cb25224f1890d5937e
Daniel West <daniel@silverback.is>
2020-01-29 13:37:01 +0000

    On same page reload attempt wait for data from first load attempt

--- packages/core/lib/core/middleware/index.js
+++ packages/core/lib/core/middleware/index.js
@@ -35,65 +35,73 @@ Middleware.routeLoader = async function({
         'Page not loading from API. Not required: already loaded current path',
         path
       )
+    await $bwstarter.$storage.state[contentModuleName].pageLoadPromise
     return
   }
   // eslint-disable-next-line no-console
   logging && console.log('Page loading ' + path)
 
   $bwstarter.$storage.commit('setCurrentRoute', [path], contentModuleName)
-
-  let routeData, response
-  try {
-    response = await $bwstarter.getRoute(path)
-    routeData = response.data
-  } catch (err) {
+  const pageLoadPromise = new Promise(async resolve => {
+    let routeData, response
     try {
-      response = await $bwstarter.fetchAndStoreLayout(null, true)
+      response = await $bwstarter.getRoute(path)
+      routeData = response.data
     } catch (err) {
+      try {
+        response = await $bwstarter.fetchAndStoreLayout(null, true)
+      } catch (err) {
+        $bwstarter.setResponseErrorPage(err)
+        return
+      }
+      process.server && Utilities.setResponseCookies(res, response)
       $bwstarter.setResponseErrorPage(err)
       return
     }
     process.server && Utilities.setResponseCookies(res, response)
-    $bwstarter.setResponseErrorPage(err)
-    return
-  }
-  process.server && Utilities.setResponseCookies(res, response)
 
-  if (!routeData) {
-    // eslint-disable-next-line no-console
-    console.warn(routeData)
-    error({
-      statusCode: 500,
-      message: 'Error fetching from API - No Route Data'
-    })
-    return
-  }
-  if (typeof routeData !== 'object') {
-    // eslint-disable-next-line no-console
-    console.warn(routeData)
-    error({ statusCode: 500, message: 'API returned invalid JSON' })
-    return
-  }
-
-  if (MAX_REDIRECTS) {
-    let redirects = 0
-    while (routeData.redirect && redirects <= MAX_REDIRECTS) {
-      routeData = routeData.redirect
-      redirects++
-    }
-    if (redirects > 0) {
-      redirect(routeData.route)
+    if (!routeData) {
+      // eslint-disable-next-line no-console
+      console.warn(routeData)
+      error({
+        statusCode: 500,
+        message: 'Error fetching from API - No Route Data'
+      })
       return
     }
-  }
-  // Sometimes the route can be returned but content is null due to the doctrine SQL Filters - database filters on content
-  if (!routeData.staticPage && !routeData.dynamicContent) {
-    error({
-      statusCode: 404,
-      message: 'Page Not Found',
-      url: 'No content available'
-    })
-    return
-  }
-  await $bwstarter.initRoute(routeData)
+    if (typeof routeData !== 'object') {
+      // eslint-disable-next-line no-console
+      console.warn(routeData)
+      error({ statusCode: 500, message: 'API returned invalid JSON' })
+      return
+    }
+
+    if (MAX_REDIRECTS) {
+      let redirects = 0
+      while (routeData.redirect && redirects <= MAX_REDIRECTS) {
+        routeData = routeData.redirect
+        redirects++
+      }
+      if (redirects > 0) {
+        redirect(routeData.route)
+        return
+      }
+    }
+    // Sometimes the route can be returned but content is null due to the doctrine SQL Filters - database filters on content
+    if (!routeData.staticPage && !routeData.dynamicContent) {
+      error({
+        statusCode: 404,
+        message: 'Page Not Found',
+        url: 'No content available'
+      })
+      return
+    }
+    await $bwstarter.initRoute(routeData)
+    resolve()
+  })
+  $bwstarter.$storage.commit(
+    'setPageLoadPromise',
+    [pageLoadPromise],
+    contentModuleName
+  )
 }
