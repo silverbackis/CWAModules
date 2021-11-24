68c15535acf4b4298b9688443b16a5699f40e921
Daniel West <daniel@silverback.is>
2021-11-24 14:16:22 +0000

    Add compoent UI when no components exist

--- packages/bulma/lib/core/pages/_base.vue
+++ packages/bulma/lib/core/pages/_base.vue
@@ -12,7 +12,21 @@
       :nested="nested"
     />
     <nuxt-child v-else-if="childKey" :key="childKey" />
-    <h1 v-else>No components or children configured for this page</h1>
+    <div v-else>
+      <h1 style="margin-bottom: 4rem;">
+        No components or children configured for this page
+      </h1>
+      <component-adder
+        v-if="$bwstarter.hasRole('ROLE_SUPER_ADMIN')"
+        :page="realPageData"
+        @add="showModal"
+      />
+      <component-modal
+        v-if="$bwstarter.hasRole('ROLE_SUPER_ADMIN')"
+        v-bind="modalProps"
+        @close="closeModal"
+      />
+    </div>
   </div>
 </template>
 
@@ -22,8 +36,35 @@ import pageMixin from '../../components/pageMixin'
 
 export default {
   components: {
-    BulmaComponents
+    BulmaComponents,
+    ComponentModal: () => import('../components/Admin/ComponentModal'),
+    ComponentAdder: () => import('../components/Admin/ComponentAdder')
   },
-  mixins: [pageMixin]
+  mixins: [pageMixin],
+  data() {
+    return {
+      componentModalActive: false,
+      modalComponentData: {}
+    }
+  },
+  computed: {
+    modalProps() {
+      return Object.assign(
+        {
+          active: this.componentModalActive
+        },
+        this.modalComponentData
+      )
+    }
+  },
+  methods: {
+    showModal({ page, location, component, remove = false }) {
+      this.componentModalActive = true
+      this.modalComponentData = { page, location, component, remove }
+    },
+    closeModal() {
+      this.componentModalActive = false
+    }
+  }
 }
 </script>
